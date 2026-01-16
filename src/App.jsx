4a039271e3d990a3d0d79fc04df5b94bd31c4394
002 <tourrec002@gmail.com>
2026-01-16 11:12:41 +0800

    modal內容格式完成

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ const closeModal=()=>{
     </div>)}
       <div ref={productModalRef} className="modal fade" id="productModal" tabIndex="-1" aria-labelledby="productModalLabel" aria-hidden="true">
         <div className="modal-dialog modal-xl">
-          <div className="modal-content">
+          {/* <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5" id="productModalLabel">Modal title</h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -181,7 +181,170 @@ const closeModal=()=>{
               <button  onClick={()=>closeModal()} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               <button type="button" className="btn btn-primary">Save changes</button>
             </div>
+          </div> */}
+          <div className="modal-content border-0">
+      <div className="modal-header bg-dark text-white">
+        <h5 id="productModalLabel" className="modal-title">
+          <span>新增產品</span>
+        </h5>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+          ></button>
+      </div>
+      <div className="modal-body">
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="mb-2">
+              <div className="mb-3">
+                <label htmlFor="imageUrl" className="form-label">
+                  輸入圖片網址
+                </label>
+                <input
+                  type="text"
+                  id="imageUrl"
+                  name="imageUrl"
+                  className="form-control"
+                  placeholder="請輸入圖片連結"
+                  />
+              </div>
+              <img className="img-fluid" src={null} alt="主圖" />
+            </div>
+            <div>
+              <div>
+					      <label htmlFor="imageUrl" className="form-label">
+					        輸入圖片網址
+					      </label>
+					      <input
+					        type="text"
+					        className="form-control"
+					        // placeholder={`圖片網址${index + 1}`}
+					      />
+					      <img
+                  className="img-fluid"
+                  src={null}
+                  // alt={`副圖${index + 1}`}
+                />
+					    </div>
+              <button className="btn btn-outline-primary btn-sm d-block w-100">
+                新增圖片
+              </button>
+            </div>
+            <div>
+              <button className="btn btn-outline-danger btn-sm d-block w-100">
+                刪除圖片
+              </button>
+            </div>
           </div>
+          <div className="col-sm-8">
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">標題</label>
+              <input
+                name="title"
+                id="title"
+                type="text"
+                className="form-control"
+                placeholder="請輸入標題"
+                />
+            </div>
+
+            <div className="row">
+              <div className="mb-3 col-md-6">
+                <label htmlFor="category" className="form-label">分類</label>
+                <input
+                  name="category"
+                  id="category"
+                  type="text"
+                  className="form-control"
+                  placeholder="請輸入分類"
+                  />
+              </div>
+              <div className="mb-3 col-md-6">
+                <label htmlFor="unit" className="form-label">單位</label>
+                <input
+                  name="unit"
+                  id="unit"
+                  type="text"
+                  className="form-control"
+                  placeholder="請輸入單位"
+                  />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="mb-3 col-md-6">
+                <label htmlFor="origin_price" className="form-label">原價</label>
+                <input
+                  name="origin_price"
+                  id="origin_price"
+                  type="number"
+                  min="0"
+                  className="form-control"
+                  placeholder="請輸入原價"
+                  />
+              </div>
+              <div className="mb-3 col-md-6">
+                <label htmlFor="price" className="form-label">售價</label>
+                <input
+                  name="price"
+                  id="price"
+                  type="number"
+                  min="0"
+                  className="form-control"
+                  placeholder="請輸入售價"
+                  />
+              </div>
+            </div>
+            <hr />
+
+            <div className="mb-3">
+              <label htmlFor="description" className="form-label">產品描述</label>
+              <textarea
+                name="description"
+                id="description"
+                className="form-control"
+                placeholder="請輸入產品描述"
+                ></textarea>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="content" className="form-label">說明內容</label>
+              <textarea
+                name="content"
+                id="content"
+                className="form-control"
+                placeholder="請輸入說明內容"
+                ></textarea>
+            </div>
+            <div className="mb-3">
+              <div className="form-check">
+                <input
+                  name="is_enabled"
+                  id="is_enabled"
+                  className="form-check-input"
+                  type="checkbox"
+                  />
+                <label className="form-check-label" htmlFor="is_enabled">
+                  是否啟用
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          data-bs-dismiss="modal"
+          onClick={() => closeModal()}
+          >
+          取消
+        </button>
+        <button type="button" className="btn btn-primary">確認</button>
+      </div>
+    </div>
         </div>
       </div>
     </>)
